5b16e8bbcd6ed8761dae8cedb0e3dd5f4c0d7394
Joaquin Fuentes <jifuentes@ubiobio.cl>
2024-09-10 14:40:00 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,57 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export function Header() {
   return (
     <Fragment>
-      <Link to={"/home"}>Home</Link>
-      <Link to={"/arbitros"}>Arbitros</Link>
-      <Link to={"/clubes"}>Clubes</Link>
-      <Link to={"/entrenadores"}>Entrenadores</Link>
-      <Link to={"/jugadores"}>Jugadores</Link>
-      <Link to={"/"}>Salir</Link>
+      <header className="bg-red d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <Link
+          to={"/"}
+          className="ms-3 d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        >
+          <img
+            src="/images/logocp.png"
+            alt="Chilean Premier League"
+            height={50}
+          />
+        </Link>
+
+        <ul class="nav nav-pills me-4">
+          <li class="nav-item">
+            <Link
+              className="nav-link text-blanco decoration-none text-20 semibold"
+              to={"/entrenadores"}
+            >
+              Entrenadores
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link
+              className="nav-link text-blanco decoration-none text-20 semibold"
+              to={"/arbitros"}
+            >
+              Arbitros
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link
+              className="nav-link text-blanco decoration-none text-20 semibold"
+              to={"/clubes"}
+            >
+              Clubes
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link
+              className="nav-link text-blanco decoration-none text-20 semibold"
+              to={"/jugadores"}
+            >
+              Jugadores
+            </Link>
+          </li>
+        </ul>
+      </header>
     </Fragment>
   );
 }
