0afcf9e8f8b1a906aca4b6f96f3fee4e6bb908b6
exerrr <maurosanjuano@gmail.com>
2024-09-14 13:13:36 -0300

    Vista entrenadores y arbitros

--- src/components/Entrenadores.jsx
+++ src/components/Entrenadores.jsx
@@ -1,12 +1,68 @@
-import React from "react";
-import { Header } from "./Header";
-
+import React, { Fragment, useState, useEffect } from "react";
+import { Header, Sidebar } from "./index";
+import { Link } from "react-router-dom";
+import "./Entrenadores.css";
 export function Entrenadores() {
+  //Obtenemos los datos
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const apiUrl = process.env.REACT_APP_API;
+
+      let data = await fetch(`${apiUrl}/entrenadores`, {
+        method: "GET",
+      }).then((response) => response.json());
+
+      setData(data);
+    }
+
+    fetchData();
+  }, []);
+  //Retorno del componente
   return (
-    <div>
-      <Header></Header>
-      <h1>Entrenadores</h1>
-      <p>Lista de Entrenadores</p>
-    </div>
+    <Fragment>
+      <div className="container-fluid">
+        <div className="row">
+          <Header></Header>
+        </div>
+        <div className="row">
+          <div className="col-2 blue">
+            <Sidebar></Sidebar>
+          </div>
+          <div className="col">
+            <div className="bg-white p-3">
+            <h2 className="red-text bold text-24">Entrenadores de la Chilean Premier League</h2>
+            <div className="entrenadores-list">
+                {data &&
+                  data.map((entrenador) => (
+                    <Link
+                      className="red-text text-12 bold decoration-none medium text-center pt-2"
+                      to={`/entrenadores/${entrenador.nombre_entrenador}`}
+                      key={entrenador.id_entrenador}
+                    >
+                      <div className="custom-border-type-entrenador">
+                        <div className="entrenador">
+                          <div className="foto-entrenador">
+                            <img src="images/Group.png" alt="Foto del entrenador" />
+                          </div>
+                          <div className="info-entrenador">
+                            <div>
+                              {entrenador.nombre_entrenador} {entrenador.apellido_entrenador}
+                            </div>
+                            <div className="bold">
+                              {entrenador.club_entrenador}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
