7eb717dbcc34fe9eaae199daddccacdd3deecd27
Joaquinubb <joaquin.fuentes2001@alumnos.ubiobio.cl>
2024-09-25 21:11:14 -0300

    tilde en arbitros

--- src/pages/Arbitros.jsx
+++ src/pages/Arbitros.jsx
@@ -30,7 +30,7 @@ export function Arbitros() {
           <div className="col mt-5 pt-4 content-container">
             <div className="bg-white p-3">
               <h2 className="red-text bold text-20">
-                Arbitros de la Chilean Premier League
+                Árbitros de la Chilean Premier League
               </h2>
               <div className="arbitros-list mt-4">
                 {data &&
